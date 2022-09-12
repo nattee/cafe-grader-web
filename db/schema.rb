6eb62ca472b207b8433cf44dc8d01ad47d0c1248
Nattee Niparnan <nattee@gmail.com>
2022-09-12 22:37:03 +0700

    utf8mb4

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,12 @@
 
 ActiveRecord::Schema.define(version: 2022_02_04_080936) do
 
-  create_table "announcements", id: :integer, charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
+  create_table "announcements", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "author"
     t.text "body"
     t.boolean "published"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.boolean "frontpage", default: false
     t.boolean "contest_only", default: false
     t.string "title"
@@ -25,112 +25,112 @@ ActiveRecord::Schema.define(version: 2022_02_04_080936) do
     t.boolean "on_nav_bar", default: false
   end
 
-  create_table "contests", id: :integer, charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
+  create_table "contests", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "title"
     t.boolean "enabled"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string "name"
   end
 
-  create_table "contests_problems", id: false, charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
+  create_table "contests_problems", id: false, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "contest_id"
     t.integer "problem_id"
   end
 
-  create_table "contests_users", id: false, charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
+  create_table "contests_users", id: false, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "contest_id"
     t.integer "user_id"
   end
 
-  create_table "countries", id: :integer, charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
+  create_table "countries", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "descriptions", id: :integer, charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
+  create_table "descriptions", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.text "body"
     t.boolean "markdowned"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "grader_configurations", id: :integer, charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
+  create_table "grader_configurations", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "key"
     t.string "value_type"
     t.string "value"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.text "description"
   end
 
-  create_table "grader_processes", id: :integer, charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
+  create_table "grader_processes", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "host"
     t.integer "pid"
     t.string "mode"
     t.boolean "active"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer "task_id"
     t.string "task_type"
     t.boolean "terminated"
-    t.index ["host", "pid"], name: "index_grader_processes_on_ip_and_pid"
+    t.index ["host", "pid"], name: "index_grader_processes_on_host_and_pid"
   end
 
-  create_table "groups", id: :integer, charset: "latin1", force: :cascade do |t|
+  create_table "groups", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name"
     t.string "description"
     t.boolean "enabled", default: true
   end
 
-  create_table "groups_problems", id: false, charset: "latin1", force: :cascade do |t|
+  create_table "groups_problems", id: false, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "problem_id", null: false
     t.integer "group_id", null: false
     t.index ["group_id", "problem_id"], name: "index_groups_problems_on_group_id_and_problem_id"
   end
 
-  create_table "groups_users", charset: "latin1", force: :cascade do |t|
+  create_table "groups_users", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "group_id", null: false
     t.integer "user_id", null: false
     t.index ["user_id", "group_id"], name: "index_groups_users_on_user_id_and_group_id"
   end
 
-  create_table "heart_beats", id: :integer, charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
+  create_table "heart_beats", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "user_id"
     t.string "ip_address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string "status"
     t.index ["updated_at"], name: "index_heart_beats_on_updated_at"
   end
 
-  create_table "languages", id: :integer, charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
+  create_table "languages", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", limit: 10
     t.string "pretty_name"
     t.string "ext", limit: 10
     t.string "common_ext"
   end
 
-  create_table "logins", id: :integer, charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
+  create_table "logins", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "user_id"
     t.string "ip_address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["user_id"], name: "index_logins_on_user_id"
   end
 
-  create_table "messages", id: :integer, charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
+  create_table "messages", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "sender_id"
     t.integer "receiver_id"
     t.integer "replying_message_id"
     t.text "body"
     t.boolean "replied"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "problems", id: :integer, charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
+  create_table "problems", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", limit: 30
     t.string "full_name"
     t.integer "full_score"
@@ -145,7 +145,7 @@ ActiveRecord::Schema.define(version: 2022_02_04_080936) do
     t.integer "difficulty"
   end
 
-  create_table "problems_tags", id: :integer, charset: "latin1", force: :cascade do |t|
+  create_table "problems_tags", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "problem_id"
     t.integer "tag_id"
     t.index ["problem_id", "tag_id"], name: "index_problems_tags_on_problem_id_and_tag_id", unique: true
@@ -153,29 +153,29 @@ ActiveRecord::Schema.define(version: 2022_02_04_080936) do
     t.index ["tag_id"], name: "index_problems_tags_on_tag_id"
   end
 
-  create_table "rights", id: :integer, charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
+  create_table "rights", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name"
     t.string "controller"
     t.string "action"
   end
 
-  create_table "rights_roles", id: false, charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
+  create_table "rights_roles", id: false, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "right_id"
     t.integer "role_id"
     t.index ["role_id"], name: "index_rights_roles_on_role_id"
   end
 
-  create_table "roles", id: :integer, charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
+  create_table "roles", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name"
   end
 
-  create_table "roles_users", id: false, charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
+  create_table "roles_users", id: false, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "role_id"
     t.integer "user_id"
     t.index ["user_id"], name: "index_roles_users_on_user_id"
   end
 
-  create_table "sessions", id: :integer, charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
+  create_table "sessions", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "session_id"
     t.text "data"
     t.datetime "updated_at"
@@ -183,24 +183,24 @@ ActiveRecord::Schema.define(version: 2022_02_04_080936) do
     t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
-  create_table "sites", id: :integer, charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
+  create_table "sites", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name"
     t.boolean "started"
     t.datetime "start_time"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer "country_id"
     t.string "password"
   end
 
-  create_table "submission_view_logs", id: :integer, charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
+  create_table "submission_view_logs", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "user_id"
     t.integer "submission_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "submissions", id: :integer, charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
+  create_table "submissions", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "user_id"
     t.integer "problem_id"
     t.integer "language_id"
@@ -224,7 +224,7 @@ ActiveRecord::Schema.define(version: 2022_02_04_080936) do
     t.index ["user_id", "problem_id"], name: "index_submissions_on_user_id_and_problem_id"
   end
 
-  create_table "tags", id: :integer, charset: "latin1", force: :cascade do |t|
+  create_table "tags", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", null: false
     t.text "description"
     t.boolean "public"
@@ -232,7 +232,7 @@ ActiveRecord::Schema.define(version: 2022_02_04_080936) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "tasks", id: :integer, charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
+  create_table "tasks", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "submission_id"
     t.datetime "created_at"
     t.integer "status"
@@ -241,15 +241,15 @@ ActiveRecord::Schema.define(version: 2022_02_04_080936) do
     t.index ["submission_id"], name: "index_tasks_on_submission_id"
   end
 
-  create_table "test_pairs", id: :integer, charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
+  create_table "test_pairs", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "problem_id"
     t.text "input", size: :medium
     t.text "solution", size: :medium
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "test_requests", id: :integer, charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
+  create_table "test_requests", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "user_id"
     t.integer "problem_id"
     t.integer "submission_id"
@@ -257,20 +257,20 @@ ActiveRecord::Schema.define(version: 2022_02_04_080936) do
     t.string "output_file_name"
     t.string "running_stat"
     t.integer "status"
-    t.datetime "updated_at", null: false
     t.datetime "submitted_at"
     t.datetime "compiled_at"
     t.text "compiler_message"
     t.datetime "graded_at"
     t.string "grader_comment"
-    t.datetime "created_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.float "running_time"
     t.string "exit_status"
     t.integer "memory_usage"
     t.index ["user_id", "problem_id"], name: "index_test_requests_on_user_id_and_problem_id"
   end
 
-  create_table "testcases", id: :integer, charset: "latin1", force: :cascade do |t|
+  create_table "testcases", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "problem_id"
     t.integer "num"
     t.integer "group"
@@ -282,15 +282,15 @@ ActiveRecord::Schema.define(version: 2022_02_04_080936) do
     t.index ["problem_id"], name: "index_testcases_on_problem_id"
   end
 
-  create_table "user_contest_stats", id: :integer, charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
+  create_table "user_contest_stats", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "user_id"
-    t.datetime "started_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.timestamp "started_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.boolean "forced_logout"
   end
 
-  create_table "users", id: :integer, charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
+  create_table "users", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "login", limit: 50
     t.string "full_name"
     t.string "hashed_password"
@@ -302,10 +302,10 @@ ActiveRecord::Schema.define(version: 2022_02_04_080936) do
     t.boolean "activated", default: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string "section"
     t.boolean "enabled", default: true
     t.string "remark"
     t.string "last_ip"
-    t.string "section"
     t.integer "default_language"
     t.index ["login"], name: "index_users_on_login", unique: true
   end
