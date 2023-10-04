35828ee08bb1582658e73add865edeb3666bb79c
Nattee Niparnan <nattee@gmail.com>
2023-10-04 14:09:17 +0700

    testcase to activestorage wip

--- app/models/testcase.rb
+++ app/models/testcase.rb
@@ -3,6 +3,9 @@ class Testcase < ApplicationRecord
   belongs_to :dataset
   #attr_accessible :group, :input, :num, :score, :sol
 
+  has_one_attached :inp_file
+  has_one_attached :ans_file
+
   def get_name_for_dir
     return code_name unless code_name.blank?
     return num.to_s
