334765c04859a625e62893077bd1ba4d8721c48d
Nattee Niparnan <nattee@gmail.com>
2024-01-15 16:20:13 +0700

    fix add user to group

--- app/controllers/user_admin_controller.rb
+++ app/controllers/user_admin_controller.rb
@@ -54,7 +54,7 @@ class UserAdminController < ApplicationController
 
 
     #add to group
-    if params[:add_to_group]
+    if params[:add_to_group] == '1'
       group = Group.find_by(id: params[:group_id])&.add_users_skip_existing(ok_user)
     end
 
