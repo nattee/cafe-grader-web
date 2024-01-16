a3f59c6a33c793aaed898b99f73fb388123dd580
Nattee Niparnan <nattee@gmail.com>
2024-01-16 17:42:15 +0700

    more verbose checker when error

--- app/engine/checker.rb
+++ app/engine/checker.rb
@@ -61,7 +61,8 @@ class Checker
           return process_result_cafe(out,err)
         end
       else
-        return report_check_error('error in checker')
+        comment = "ERROR IN CHECKER!!!\n-- stderr --\n#{err}-- status -- #{status}"
+        return report_check_error(comment)
       end
     else
       return report_check_error('unknown evaluation type')
@@ -87,7 +88,9 @@ class Checker
       color = :red
     end
 
-    Rainbow(result[:result].to_s).color(color)
+    res = Rainbow(result[:result].to_s).color(color)
+    res += " comment: #{result[:comment]}" unless result[:comment].blank?
+    return res
   end
 
   # main run function
