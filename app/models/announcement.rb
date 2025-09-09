cc71ce980bb02101b1ddf8764db486d3d52d43d9
Nattee Niparnan <nattee@gmail.com>
2025-09-09 22:18:53 +0700

    wip

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -2,7 +2,15 @@ class Announcement < ApplicationRecord
   has_one_attached :file
   belongs_to :group, optional: true
 
-  def self.published(contest_started=false)
+  scope :for_user, ->(user) {
+    where(group: nil).or(where(group: user.groups_for_action(:submit)))
+  }
+
+  scope :published, -> { where(published: true) }
+  scope :frontpage, -> { published.where(frontpage: true) }
+  scope :mainpage, -> { publisehd.where(frontpage: false) }
+
+  def self.published(contest_started = false)
     if contest_started
       where(published: true).where(frontpage: false).order(created_at: :desc)
     else
@@ -13,5 +21,4 @@ class Announcement < ApplicationRecord
   def self.frontpage
     where(published: 1).where(frontpage: 1).order(created_at: :desc)
   end
-
 end
