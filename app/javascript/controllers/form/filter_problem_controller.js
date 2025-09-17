9b4cc83aedc2711bef7a5cc637df66928fc1721f
Nattee Niparnan <nattee@gmail.com>
2025-09-17 17:48:48 +0700

    wip

---
+++ app/javascript/controllers/form/filter_problem_controller.js
@@ -0,0 +1,66 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [ "useOption", "groupSelect", "idSelect", "tagSelect" ]
+
+  connect() {
+    // Dispatch the initial state so listeners can load with the default values
+    this.filterChanged()
+
+    $('.select2').on('change.select2', () => {
+      this.dispatchChange();
+    });
+  }
+
+  /**
+   * This action is called any time an input changes.
+   */
+  filterChanged() {
+    this.toggleSelects()
+    this.dispatchChange()
+  }
+
+  /**
+   * A UX helper to enable the relevant select box and disable the others.
+   */
+  toggleSelects() {
+    const selectedValue = this.selectedOptionValue;
+    $(this.idSelectTarget).prop('disabled', selectedValue !== 'ids').trigger('change');
+    $(this.groupSelectTarget).prop('disabled', selectedValue !== 'groups').trigger('change');
+    $(this.tagSelectTarget).prop('disabled', selectedValue !== 'tags').trigger('change');
+  }
+
+  /**
+   * Dispatches a custom event with the current filter parameters.
+   *   instead of firing a real event, we decided to store it
+   *   dirtily on window.problemFilterParams
+   *
+   *   window.problemFilterParams then can be used by non-stimulus JS such as DataTables.ajax
+   */
+  dispatchChange() {
+    console.log('yyy')
+    window.problemFilterParams = this.params
+  }
+
+  /**
+   * Getter for the value of the currently selected radio button.
+   * @returns {string}
+   */
+  get selectedOptionValue() {
+    return this.useOptionTargets.find(radio => radio.checked)?.value;
+  }
+
+  /**
+   * A getter that builds and returns an object of the current filter values.
+   * @returns {object}
+   */
+  get params() {
+    window.xxx = this.isSelectTarget
+    return {
+      'probs[use]': this.selectedOptionValue,
+      'probs[ids][]': $(this.idSelectTarget).val(),
+      'probs[group_ids][]': $(this.groupSelectTarget).val(),
+      'probs[tag_ids][]': $(this.tagSelectTarget).val()
+    };
+  }
+}
