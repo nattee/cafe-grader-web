e02c7e0ecf04b0c00b9a8e336f4c5480309fb77e
Nattee Niparnan <nattee@gmail.com>
2026-03-31 21:31:24 +0700

    modernize grader_process page

--- app/helpers/graders_helper.rb
+++ app/helpers/graders_helper.rb
@@ -3,4 +3,9 @@ module GradersHelper
     return 'all' if job_type.blank? || job_type == 'compile evaluate score'
     return job_type
   end
+
+  def job_type_badges(job_type)
+    return ['all'] if job_type.blank? || job_type.strip == 'compile evaluate score'
+    job_type.split
+  end
 end
