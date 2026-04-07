238e0c2a1ff49e50407863dcccb19335d1cd8c87
Nattee Niparnan <nattee@gmail.com>
2026-04-08 06:51:31 +0700

    add more system test for CRUD

---
+++ test/system/tags_test.rb
@@ -0,0 +1,33 @@
+require "application_system_test_case"
+
+class TagsTest < ApplicationSystemTestCase
+  test "create new tag" do
+    login("admin", "admin")
+    visit tags_path
+
+    click_on "New Tag"
+    fill_in "Name", with: "medium"
+    click_on "Create Tag"
+
+    assert_text "Tag was successfully created."
+  end
+
+  test "update tag" do
+    login("admin", "admin")
+    visit edit_tag_path(tags(:tag_easy))
+
+    fill_in "Name", with: "beginner"
+    click_on "Save Changes"
+
+    assert_text "Tag beginner was successfully updated."
+  end
+
+  private
+
+  def login(username, password)
+    visit root_path
+    fill_in "Login", with: username
+    fill_in "Password", with: password
+    click_on "Login"
+  end
+end
